b581ced58a5d591b8cac5e0a65e5d9d4859be63f
MadsJaeger <mhh@securecapital.dk>
2021-10-13 13:35:25 +0200

    Added tests on invoice, user, application_record, and application_record_logger

--- test/application_record_logger_test.rb
+++ test/application_record_logger_test.rb
@@ -4,4 +4,17 @@ class ApplicationRecordLoggerTest < ActiveSupport::TestCase
   test "it has a version number" do
     assert ApplicationRecordLogger::VERSION
   end
+
+  test "it has config" do
+    assert_instance_of Hash, ApplicationRecordLogger.config
+    assert_equal ApplicationRecordLogger.config, ApplicationRecordLogger.class_variable_get("@@config")
+    assert_equal ApplicationRecordLogger.config[:log_create], true
+    assert_equal ApplicationRecordLogger.config[:log_update], true
+    assert_equal ApplicationRecordLogger.config[:log_destroy], true
+    assert_equal ApplicationRecordLogger.config[:log_create_data], false
+    assert_equal ApplicationRecordLogger.config[:log_user_activity_only], true
+    assert_equal ApplicationRecordLogger.config[:log_fields], []
+    assert_equal ApplicationRecordLogger.config[:log_field_types], %i(string date integer decimal float datetime)
+    assert_equal ApplicationRecordLogger.config[:log_exclude_field_names], %i(id updated_at created_at)
+  end
 end
--- test/dummy/test/models/user_test.rb
+++ test/dummy/test/models/user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "has defualt logging_fields" do
+    assert_equal User.logging_options[:log_fields], ["name", "email"]
+  end
 end
