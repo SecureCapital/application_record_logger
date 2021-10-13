2dd5eef14fe9c7d8399f98b7b8d83f2709775f08
MadsJaeger <mhh@securecapital.dk>
2021-10-13 13:34:35 +0200

    Modified config variables to duplicate from @@config on module, but be class instqnce variables on included classes to allow differential in configuration between classes. Removed boolean logic on when to create a log instance and what data this should hold to LogService. This is now a procedure for creating a ApplicarionRecordLog. This ensures less antanglemewnt of the module into the including classes. Likevwise is logic removed from ApplicationRecordLog. By holding the log service separately may the user skip callbacks, and handle the logging directly with the saved object and the logger service.

--- app/models/application_record_log.rb
+++ app/models/application_record_log.rb
@@ -16,7 +16,7 @@ class ApplicationRecordLog < ApplicationRecord
     false
   end
 
-  belongs_to :owner, polymorphic: true, optional: false
+  belongs_to :record, polymorphic: true, optional: false
   belongs_to :user, optional: true
   serialize :data
   enum :action => {
@@ -25,55 +25,36 @@ class ApplicationRecordLog < ApplicationRecord
     :db_destroy => 2,
   }
 
-  def self.log(record:, user:, action:)
-    if record && (record.saved_changes? || record.destroyed?)
-      user_id = user.respond_to?(:id) ? user.id : user
-      if action == :db_create
-        data = (record.class.log_create_data ||  where(owner: record, action: :db_create).any?) ? record.logging_data : nil
-      elsif action == :db_destroy
-        data = record.attributes.map do |key, value|
-          [key, [value, nil]]
-        end.to_h.slice(*record.class.log_fields)
+  class << self
+    def rollback(record:, record_type:, record_id:, timepoint:, step:)
+      klass       = record_type.constantize
+      given_record = record
+      given_record ||= klass.find_by(id: record_id)
+      given_record ||= klass.new(id: record_id)
+      logs = if step
+        where(
+          record: record,
+        ).order(:updated_at => :desc).limit(step)
       else
-        data = record.logging_data
+        where(
+          record: record,
+          updated_at: timepoint..(DateTime::Infinity.new)
+        ).order(:updated_at => :desc)
       end
-      create(
-        owner: record,
-        data: data,
-        user_id: user_id,
-        action: action,
-      )
-    end
-  end
-
-  def self.rollback(owner:, owner_type:, owner_id:, timepoint:, step:)
-    klass       = owner_type.constantize
-    given_owner = owner
-    given_owner ||= klass.find_by(id: owner_id)
-    given_owner ||= klass.new(id: owner_id)
-    logs = if step
-      where(
-        owner: owner,
-      ).order(:updated_at => :desc).limit(step)
-    else
-      where(
-        owner: owner,
-        updated_at: timepoint..(DateTime::Infinity.new)
-      ).order(:updated_at => :desc)
-    end
-    if timepoint && (timepoint < where(owner: owner).minimum(:updated_at))
-      return klass.new(id: owner_id)
-    else
-      logs.each do |log|
-        (log.data||{}).each do |key, arr|
-          given_owner.send("#{key}=", arr[0])
+      if timepoint && (timepoint < where(record: record).minimum(:updated_at))
+        return klass.new(id: record_id)
+      else
+        logs.each do |log|
+          (log.data||{}).each do |key, arr|
+            given_record.send("#{key}=", arr[0])
+          end
         end
+        given_record
       end
-      given_owner
     end
-  end
 
-  def self.rollback!(**kwargs)
-    rollback(**kwargs).save
+    def rollback!(**kwargs)
+      rollback(**kwargs).save
+    end
   end
 end
--- lib/application_record_logger.rb
+++ lib/application_record_logger.rb
@@ -1,94 +1,90 @@
 require "application_record_logger/engine"
 require "application_record_logger/version"
+require "application_record_logger/log_service"
 
 module ApplicationRecordLogger
+  @@config = {
+    log_create: true,
+    log_update: true,
+    log_destroy: true,
+    log_create_data: false,
+    log_user_activity_only: true,
+    log_fields: [],
+    log_field_types: %i(string date integer decimal float datetime),
+    log_exclude_field_names: %i(id updated_at created_at)
+  }
+
   def self.config
-    @@config ||= {
-      log_field_types: %i(string date integer decimal float datetime),
-      exclude_field_names: %w(id updated_at created_at)
-    }
+    @@config
   end
 
   def self.included(base)
     base.extend ClassMethods
     base.class_eval do
-      has_many :application_record_logs, as: :owner
+      has_many :application_record_logs, as: :record
 
       after_create do
-        create_application_record_log!(:db_create) if self.class.log_create
+        create_application_record_log(:db_create)
       end
 
       after_update do
-        create_application_record_log!(:db_update) if self.class.log_update
+        create_application_record_log(:db_update)
       end
 
       after_destroy do
-        create_application_record_log!(:db_destroy) if self.class.log_destroy
+        create_application_record_log(:db_destroy)
       end
     end
   end
 
   module ClassMethods
-    def default_logging_fields
-      @@default_logging_fields ||= columns_hash.map do |key,rec|
-        [key, rec.type]
-      end.select do |key,type|
-        ApplicationRecordLogger.config[:log_field_types].include?(type)
-      end.reject do |key, type|
-        ApplicationRecordLogger.config[:exclude_field_names].include?(key)
-      end.map(&:first)
+    def default_log_fields
+      if table_name.blank?
+        []
+      else
+        field_types = logging_options[:log_field_types]
+        exclude_names = logging_options[:log_exclude_field_names]
+
+        columns_hash.map do |key,rec|
+          [key, rec.type]
+        end.select do |key,type|
+          (field_types == [:any])||field_types.include?(type.to_sym)
+        end.reject do |key, type|
+          exclude_names.include?(key.to_sym)
+        end.map(&:first)
+      end
     end
 
     def logging_options
-      {
-        log_create: true,
-        log_update: true,
-        log_destroy: true,
-        log_fields: default_logging_fields,
-        log_create_data: false,
-        log_user_activity_only: true,
-      }
+      @logging_options ||= set_logging_options
     end
 
-    %i(log_create log_update log_destroy log_fields log_create_data log_user_activity_only).each do |key|
-      define_method(key) do
-        logging_options[key]
+    def configure_logging_options(&block)
+      set_logging_options(&block)
+    end
+
+    private
+    def set_logging_options(&block)
+      @logging_options = ApplicationRecordLogger.config.dup
+      yield @logging_options if block_given?
+      if @logging_options[:log_fields].empty?
+        @logging_options[:log_fields] = default_log_fields
       end
+      @logging_options
     end
   end
 
   attr_accessor :current_user
-
-  def logging_data
-    saved_changes.slice(*self.class.log_fields)
-  end
-
-  def user_and_log_user_only?
-    if self.class.log_user_activity_only
-      current_user && current_user.id
-    else
-      true
-    end
-  end
-
-  def log_create?
-    self.class.log_create && user_and_log_user_only?
-  end
-
-  def log_update?
-    self.class.log_update && user_and_log_user_only?
-  end
-
-  def log_destroy?
-    self.class.log_destroy && user_and_log_user_only?
-  end
-
   private
-  def create_application_record_log!(action)
-    ApplicationRecordLog.log(
+  def create_application_record_log(action)
+    res = ApplicationRecordLogger::LogService.call(
       record: self,
       user: current_user,
       action: action,
+      # config: self.class.logging_options # implicit!
     )
+    if res && application_record_logs.loaded
+      application_record_logs << res
+    end
   end
 end
